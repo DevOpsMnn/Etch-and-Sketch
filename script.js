9d5aa2202f3fb4986e72dafa0f0cda2b53cff664
DevOpsMnn <tim.moonen@proton.me>
2022-09-17 19:48:30 +0200

    Added a button in HTML and added functionality to javascript to adjust the amount of grid items to a custom value

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-var container = document.getElementById("container");
+let listenUserAmount = document.getElementById("btn-square-selector");
+let container = document.getElementById("container");
+let mainWrapper = document.getElementById("main-wrapper")
 
-amount = 16*16
+let amount = 16*16
 
-for (let i=0; i<amount; i++) {
+for (let i=0; i < amount; i++) {
     var cell = document.createElement("div");
     cell.setAttribute('class', 'blank');
     cell.setAttribute('id', i);
     container.appendChild(cell);
 }
+
+listenUserAmount.addEventListener('click', () => {
+
+    let choosenAmount = prompt("Hoeveel vierkantjes wil je horizontaal? Graag onder de 100 blijven");
+    if (choosenAmount > 100) {
+        alert("Je hebt meer dan 100 ingevult, probeer het opnieuw.");
+        return;
+    } else {
+        amount = choosenAmount * choosenAmount;
+
+        container.remove();
+        container = document.createElement("div");
+        container.setAttribute('id', 'container');
+        document.getElementById("main-wrapper").appendChild(container);
+        document.getElementById("container").style.gridTemplateColumns = "repeat(" + choosenAmount + ", auto)";
+        
+        for (let i=0; i<amount; i++) {
+            var cell = document.createElement("div");
+            cell.setAttribute('class', 'blank');
+            cell.setAttribute('id', i);
+            container.appendChild(cell);
+        }
+    }
+});
