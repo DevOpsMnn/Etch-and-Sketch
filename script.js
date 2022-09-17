d657ebf9e552a086a3bca7337be149543bb44e6e
DevOpsMnn <tim.moonen@proton.me>
2022-09-17 18:11:02 +0200

    Added a border, class for blank divs and a hover state to change the color to black

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ amount = 16*16
 
 for (let i=0; i<amount; i++) {
     var cell = document.createElement("div");
+    cell.setAttribute('class', 'blank');
     cell.setAttribute('id', i);
-    cell.innerHTML = i;
     container.appendChild(cell);
 }
